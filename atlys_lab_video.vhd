827f4d676f34899bae9a3b9f3fca5665d90f03db
John Miller <c15john.miller@usafa.edu>
2014-02-04 08:31:57 -0700

    Created the top shell, have not tested it yet, consulted with C2C Mossing as to how he wrote his top shell, sounds similar to mine, will test within the next 1.5 hours

---
+++ atlys_lab_video.vhd
@@ -0,0 +1,173 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    02:57:24 02/04/2014 
+-- Design Name: 
+-- Module Name:    atlys_lab_video - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+ use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+ library UNISIM;
+ use UNISIM.VComponents.all;
+
+entity atlys_lab_video is
+    Port ( clk : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           tmds : out  STD_LOGIC_VECTOR (3 downto 0);
+           tmdsb : out  STD_LOGIC_VECTOR (3 downto 0));
+end atlys_lab_video;
+
+
+
+
+architecture Miller of atlys_lab_video is
+
+COMPONENT dvid
+	PORT(
+		clk : IN std_logic;
+		clk_n : IN std_logic;
+		clk_pixel : IN std_logic;
+		red_p : IN std_logic_vector(7 downto 0);
+		green_p : IN std_logic_vector(7 downto 0);
+		blue_p : IN std_logic_vector(7 downto 0);
+		blank : IN std_logic;
+		hsync : IN std_logic;
+		vsync : IN std_logic;          
+		red_s : OUT std_logic;
+		green_s : OUT std_logic;
+		blue_s : OUT std_logic;
+		clock_s : OUT std_logic
+		);
+	END COMPONENT;
+
+
+	COMPONENT pixel_gen
+	PORT(
+		row : IN unsigned(10 downto 0);
+		column : IN unsigned(10 downto 0);
+		blank : IN std_logic;          
+		r : OUT unsigned(7 downto 0);
+		g : OUT unsigned(7 downto 0);
+		b : OUT unsigned(7 downto 0)
+		);
+	END COMPONENT;
+
+	COMPONENT vga_sync
+	PORT(
+		clk : IN std_logic;
+		reset : IN std_logic;          
+		h_sync : OUT std_logic;
+		v_sync : OUT std_logic;
+		v_completed : OUT std_logic;
+		blank : OUT std_logic;
+		row : OUT unsigned(10 downto 0);
+		column : OUT unsigned(10 downto 0)
+		);
+	END COMPONENT;
+	
+	signal row_sig, col_sig : unsigned (10 downto 0);
+	signal  pixel_clk, serialize_clk, serialize_clk_n, h_sync, v_sync, v_completed, blank, clock_s, red_s, green_s, blue_s : std_logic;
+	signal red, blue, green : unsigned (7 downto 0);
+begin
+
+Inst_vga_sync: vga_sync PORT MAP(
+		clk => clk,
+		reset => reset,
+		h_sync => h_sync,
+		v_sync => v_sync,
+		v_completed => v_completed,
+		blank => blank,
+		row => row_sig,
+		column => col_sig
+	);
+
+ Inst_pixel_gen: pixel_gen PORT MAP(
+		row => row_sig,
+		column => col_sig,
+		blank => blank,
+		r => red,
+		g => green,
+		b => blue
+	);
+ 
+ 
+ -- Clock divider - creates pixel clock from 100MHz clock
+   	inst_DCM_pixel: DCM
+    generic map(
+                   CLKFX_MULTIPLY => 2,
+                   CLKFX_DIVIDE   => 8,
+                   CLK_FEEDBACK   => "1X"
+               )
+    port map(
+                clkin => clk,
+                rst   => reset,
+                clkfx => pixel_clk
+            );
+
+    -- Clock divider - creates HDMI serial output clock
+    inst_DCM_serialize: DCM
+    generic map(
+                   CLKFX_MULTIPLY => 10, -- 5x speed of pixel clock
+                   CLKFX_DIVIDE   => 8,
+                   CLK_FEEDBACK   => "1X"
+               )
+    port map(
+                clkin => clk,
+                rst   => reset,
+                clkfx => serialize_clk,
+                clkfx180 => serialize_clk_n
+            );
+
+    -- TODO: VGA component instantiation
+    -- TODO: Pixel generator component instantiation
+
+    -- Convert VGA signals to HDMI (actually, DVID ... but close enough)
+    inst_dvid: entity work.dvid
+    port map(
+                clk       => serialize_clk,
+                clk_n     => serialize_clk_n, 
+                clk_pixel => pixel_clk,
+                red_p     => std_logic_vector(red),
+                green_p   => std_logic_vector(green),
+                blue_p    => std_logic_vector(blue),
+                blank     => blank,
+                hsync     => h_sync,
+                vsync     => v_sync,
+                -- outputs to TMDS drivers
+                red_s     => red_s,
+                green_s   => green_s,
+                blue_s    => blue_s,
+                clock_s   => clock_s
+            );
+
+    -- Output the HDMI data on differential signalling pins
+    OBUFDS_blue  : OBUFDS port map
+        ( O  => TMDS(0), OB => TMDSB(0), I  => blue_s  );
+    OBUFDS_red   : OBUFDS port map
+        ( O  => TMDS(1), OB => TMDSB(1), I  => green_s );
+    OBUFDS_green : OBUFDS port map
+        ( O  => TMDS(2), OB => TMDSB(2), I  => red_s   );
+    OBUFDS_clock : OBUFDS port map
+        ( O  => TMDS(3), OB => TMDSB(3), I  => clock_s );
+
+end miller;
+
