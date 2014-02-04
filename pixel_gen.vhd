f425b2cdf0211aad489e2374f6fded74e78de465
John Miller <c15john.miller@usafa.edu>
2014-02-03 23:12:39 -0700

    Created pixel_gen module, have not tested, although it passes syntax checks

--- pixel_gen.vhd
+++ pixel_gen.vhd
@@ -40,8 +40,57 @@ end pixel_gen;
 
 architecture Behavioral of pixel_gen is
 
+type color_type is
+(black, red, blue, green, yellow, white);
+
+signal color_result: color_type;
 begin
 
+
+process(blank)
+	begin 
+	color_result <= black;
+	
+	if(blank = '1') then
+		color_result <= black;
+	else 
+		color_result <=red;
+	end if;
+end process;
+
+process(color_result)
+begin 
+	r <= 0;
+	g <= 0;
+	b <= 0;
+	
+	case color_result is
+		when red =>
+			r <= 255;
+			g <= 0;
+			b <= 0;
+		when green =>
+			r <= 0;
+			g <= 255;
+			b <= 0;
+		when blue =>
+			r <=0;
+			g <=0;
+			b <=255;
+		when black =>
+			r <= 0;
+			g <= 0;
+			b <= 0;
+		when white =>
+			r <= 255;
+			g <= 255;
+			b <= 255;
+		when yellow =>
+			r <= 0;
+			g <= 255;
+			b <= 255;
+		end case;
+ end process;
 
 end Behavioral;
 
