7ec174874710c56ead51919000958a13d94af7bb
John Miller <c15john.miller@usafa.edu>
2014-02-04 17:07:34 -0700

    Finished up to A functionality, changed optimization to area reduction, which is probably indicitive of poor coding

--- atlys_lab_video.vhd
+++ atlys_lab_video.vhd
@@ -32,6 +32,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity atlys_lab_video is
     Port ( clk : in  STD_LOGIC;
            reset : in  STD_LOGIC;
+			  SW6: in STD_LOGIC;
+			  SW7: in STD_LOGIC;
            tmds : out  STD_LOGIC_VECTOR (3 downto 0);
            tmdsb : out  STD_LOGIC_VECTOR (3 downto 0));
 end atlys_lab_video;
@@ -45,7 +47,9 @@ architecture Miller of atlys_lab_video is
 	PORT(
 		row : IN unsigned(10 downto 0);
 		column : IN unsigned(10 downto 0);
-		blank : IN std_logic;          
+		blank : IN std_logic;   
+		switch_6 : in std_logic;
+		switch_7 : in std_logic;
 		r : OUT std_logic_vector(7 downto 0);
 		g : OUT std_logic_vector(7 downto 0);
 		b : OUT std_logic_vector(7 downto 0)
@@ -85,6 +89,8 @@ Inst_vga_sync: vga_sync PORT MAP(
 		row => row_sig,
 		column => col_sig,
 		blank => blank,
+		switch_6 => SW6,
+		switch_7 => SW7,
 		r => red,
 		g => green,
 		b => blue
--- pixel_gen.vhd
+++ pixel_gen.vhd
@@ -33,6 +33,8 @@ entity pixel_gen is
     Port ( row : in unsigned (10 downto 0);
            column : in  unsigned (10 downto 0);
            blank : in  STD_LOGIC;
+			  switch_6 : in std_logic;
+			  switch_7 : in std_logic;
            r : out  std_logic_vector (7 downto 0);
            g : out  std_logic_vector (7 downto 0);
            b : out  std_logic_vector (7 downto 0)
@@ -58,25 +60,83 @@ begin
 	g <= ( others=>'0');
 	b <= ( others=>'0');
 
-	elsif ( row > 511) then
-			r <= ( others=>'0');
-			g <=(others => '1');
-			b <= (others => '1');
-	elsif(column < 160) then
-			r <= ( others=>'0');
-			g <= ( others=>'0');
-			b <= (others => '1');
-	elsif (column < 320) then
-			r <= ( others=>'0');
-			g <=  (others => '1');
-			b <= ( others=>'0');					
- else 
-			r <= ( others=>'0');
-			g <= ( others=>'0');
-			b <= (others => '1');
-	end if; 
+	elsif(switch_6 = '0' and switch_7 = '0') then
+					if ( row > 320)  then
+							r <= ( others=>'1');
+							g <=(others => '1');
+							b <= (others => '0');
+					elsif(column < 214) then
+							r <= ( others=>'1');
+							g <= ( others=>'0');
+							b <= (others => '0');
+					elsif (column < 427) then
+							r <= ( others=>'0');
+							g <=  (others => '1');
+							b <= ( others=>'0');					
+					else 
+							r <= ( others=>'0');
+							g <= ( others=>'0');
+							b <= (others => '1');
+					end if;
+	
 				
+		elsif(switch_6 = '0' and switch_7 = '1') then
+					if ( row > 320)  then
+							r <= ( others=>'0');
+							g <=(others => '0');
+							b <= (others => '1');
+					elsif(column < 214) then
+							r <= ( others=>'1');
+							g <= ( others=>'1');
+							b <= (others => '0');
+					elsif (column < 427) then
+							r <= ( others=>'0');
+							g <=  (others => '0');
+							b <= ( others=>'1');					
+					else 
+							r <= ( others=>'1');
+							g <= ( others=>'0');
+							b <= (others => '0');
+					end if;
+	
+		elsif(switch_6 = '1' and switch_7 = '0') then
+					if ( row > 320)  then
+							r <= ( others=>'0');
+							g <=(others => '1');
+							b <= (others => '0');
+					elsif(column < 214) then
+							r <= ( others=>'1');
+							g <= ( others=>'0');
+							b <= (others => '0');
+					elsif (column < 427) then
+							r <= ( others=>'1');
+							g <=  (others => '1');
+							b <= ( others=>'0');					
+					else 
+							r <= ( others=>'0');
+							g <= ( others=>'0');
+							b <= (others => '1');
+					end if;
 				
+		elsif(switch_6 = '1' and switch_7 = '1') then
+					if ( row > 320)  then
+							r <= ( others=>'1');
+							g <=(others => '0');
+							b <= (others => '1');
+					elsif(column < 214) then
+							r <= ( others=>'1');
+							g <= ( others=>'1');
+							b <= (others => '0');
+					elsif (column < 427) then
+							r <= ( others=>'0');
+							g <=  (others => '0');
+							b <= ( others=>'1');					
+					else 
+							r <= ( others=>'1');
+							g <= ( others=>'0');
+							b <= (others => '0');
+					end if;
+	end if; 			
 end process;
 
 
